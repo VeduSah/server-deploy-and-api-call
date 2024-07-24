3ea495f39eefb0e820f9a94db9a43ec747793a57
VedSah <vedantsahani6@gmail.com>
2024-07-24 07:26:49 +0530

    chanege

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log('request has arrived')
     const codingApi= await axios.get('https://api.codingninjas.com/api/v3/event_tags')
     console.log(codingApi.data.data)
 
-    res.setHeader('Content-type','apllication/json')
+    // res.setHeader('Content-type','apllication/json')
     res.end(JSON.stringify(codingApi.data.data))
 })
 server.listen(port,()=>console.log(`Server is listening at port:${port}`));
