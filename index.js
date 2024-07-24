f7bf0d64ebb32207e8ea68c2b3e402f005161f30
VedSah <vedantsahani6@gmail.com>
2024-07-24 06:51:19 +0530

    Change

---
+++ index.js
@@ -0,0 +1,12 @@
+const http=require('http');
+const axios=require('axios');
+const port=3000;
+const server=http.createServer(async (req,res)=>{
+console.log('request has arrived')
+    const codingApi= await axios.get('https://api.codingninjas.com/api/v3/event_tags')
+    console.log(codingApi.data.data)
+
+    res.setHeader('Content-type','apllication/json')
+    res.end(JSON.stringify(codingApi.data.data))
+})
+server.listen(port,()=>console.log(`Server is listening at port:${port}`));
